85685715538bf1b7d792c7acb0fbc0800834d4af
Jay <jamied_uk@hotmail.com>
2024-09-15 12:37:27 +0100

    Add files via upload

--- ehm.sh
+++ ehm.sh
@@ -143,6 +143,192 @@ display_agreement() {
     fi
 }
 
+# Function to display and execute exploitation commands
+exploitation_menu() {
+    clear
+    echo "### Exploitation Commands ###"
+    echo "1. ssh"
+    echo "2. git"
+    echo "3. nc"
+    echo "4. telnet"
+    echo "5. exploitdb"
+    echo "6. searchsploit"
+    echo "7. <Include options for privilege escalation exploits>"
+    echo "8. Return to Main Menu"
+    echo "------------------"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) custom_command "ssh" ;;
+        2) custom_command "git" ;;
+        3) custom_command "nc" ;;
+        4) custom_command "telnet" ;;
+        5) custom_command "<exploitdb command>" ;;
+        6) custom_command "searchsploit" ;;
+        7) custom_command "<Privilege Escalation Exploit command>" ;;
+        8) main_menu ;;
+        *) echo "Invalid choice. Please enter a number between 1 and 8." && sleep 2 ;;
+    esac
+}
+
+# Function to display and execute post-exploitation commands
+post_exploitation_menu() {
+    clear
+    echo "### Post-Exploitation Commands ###"
+    echo "1. nc -e /bin/sh"
+    echo "2. nc -lvp"
+    echo "3. nc -v"
+    echo "4. ssh -D"
+    echo "5. ssh user@remote_host"
+    echo "6. sudo chmod +s /bin/bash"
+    echo "7. <Include more post-exploitation frameworks like Empire or Cobalt Strike>"
+    echo "8. <Include tools for data exfiltration, lateral movement, and persistence>"
+    echo "9. Return to Main Menu"
+    echo "------------------"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) custom_command "nc -e /bin/sh" ;;
+        2) custom_command "nc -lvp" ;;
+        3) custom_command "nc -v" ;;
+        4) custom_command "ssh -D" ;;
+        5) custom_command "ssh user@remote_host" ;;
+        6) custom_command "sudo chmod +s /bin/bash" ;;
+        7) custom_command "<Post-Exploitation Framework command>" ;;
+        8) custom_command "<Data Exfiltration command>" ;;
+        9) main_menu ;;
+        *) echo "Invalid choice. Please enter a number between 1 and 9." && sleep 2 ;;
+    esac
+}
+
+
+# Function to display and execute miscellaneous commands
+miscellaneous_menu() {
+    clear
+    echo "### Miscellaneous Commands ###"
+    echo "1. cat /dev/urandom"
+    echo "2. iftop"
+    echo "3. ptunnel"
+    echo "4. tcpdump -i any icmp"
+    echo "5. curl"
+    echo "6. curl with custom header"
+    echo "7. nikto"
+    echo "8. wget"
+    echo "9. gobuster"
+    echo "10. tshark"
+    echo "11. tmux"
+    echo "12. timeout"
+    echo "13. grep"
+    echo "14. alias"
+    echo "15. hexedit"
+    echo "16. Bless (hex editor)"
+    echo "17. Midnight Commander (mc)"
+    echo "18. Vim (text editor)"
+    echo "19. nano (text editor)"
+    echo "20. aircrack-ng"
+    echo "21. reaver"
+    echo "22. setoolkit"
+    echo "23. gophish"
+    echo "24. Return to Main Menu"
+    echo "------------------"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) custom_command "cat /dev/urandom" ;;
+        2) custom_command "iftop" ;;
+        3) custom_command "ptunnel" ;;
+        4) custom_command "tcpdump -i any icmp" ;;
+        5) custom_command "curl" ;;
+        6) custom_command "curl with custom header" ;;
+        7) custom_command "nikto" ;;
+        8) custom_command "wget" ;;
+        9) custom_command "gobuster" ;;
+        10) custom_command "tshark" ;;
+        11) custom_command "tmux" ;;
+        12) custom_command "timeout" ;;
+        13) custom_command "grep" ;;
+        14) custom_command "alias" ;;
+        15) custom_command "hexedit" ;;
+        16) custom_command "bless" ;;
+        17) custom_command "mc" ;;
+        18) custom_command "vim" ;;
+        19) custom_command "nano" ;;
+        20) custom_command "aircrack-ng" ;;
+        21) custom_command "reaver" ;;
+        22) custom_command "setoolkit" ;;
+        23) custom_command "gophish" ;;
+        24) main_menu ;;
+        *) echo "Invalid choice. Please enter a number between 1 and 24." && sleep 2 ;;
+    esac
+}
+
+# Function to display and execute analysis commands
+analysis_menu() {
+    clear
+    echo "### Analysis Commands ###"
+    echo "1. tcpdump -w"
+    echo "2. tcpdump -e ip.src -e ip.dst -e frame.protocols -T fields -r"
+    echo "3. tcpdump -r -q -z follow,tcp,ascii,0"
+    echo "4. wpscan"
+    echo "5. <Include more tools for log analysis like logstash, splunk, or graylog>"
+    echo "6. Install Volatility Memory Forensics tool"
+    echo "7. Return to Main Menu"
+    echo "------------------"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) custom_command "tcpdump -w" ;;
+        2) custom_command "tcpdump -e ip.src -e ip.dst -e frame.protocols -T fields -r" ;;
+        3) custom_command "tcpdump -r -q -z follow,tcp,ascii,0" ;;
+        4) custom_command "wpscan" ;;
+        5) custom_command "<Log Analysis tool command>" ;;
+        6) install_volatility ;;
+        7) main_menu ;;
+        *) echo "Invalid choice. Please enter a number between 1 and 7." && sleep 2 ;;
+    esac
+}
+
+
+# Function to display and execute cracking commands
+cracking_menu() {
+    clear
+    echo "### Cracking Commands ###"
+    echo "1. Install Seclists"
+    echo "2. hashcat"
+    echo "3. John the Ripper"
+    echo "4. Hydra"
+    echo "5. Medusa"
+    echo "6. Crowbar"
+    echo "7. RainbowCrack"
+    echo "8. Crunch"
+    echo "9. CeWL"
+    echo "10. RarCrack"
+    echo "11. Hash-Identifier"
+    echo "12. Pass the Hash Toolkit"
+    echo "13. Jack the Ripper"
+    echo "14. Return to Main Menu"
+    echo "------------------"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) custom_command "sudo apt install -y seclists" ;;
+        2) custom_command "hashcat" ;;
+        3) custom_command "john" ;;
+        4) custom_command "hydra" ;;
+        5) custom_command "medusa" ;;
+        6) custom_command "crowbar" ;;
+        7) custom_command "rainbowcrack" ;;
+        8) custom_command "crunch" ;;
+        9) custom_command "cewl" ;;
+        10) custom_command "rarcrack" ;;
+        11) custom_command "hash-identifier" ;;
+        12) custom_command "pth-toolkit" ;;  # Added "Pass the Hash Toolkit"
+        13) custom_command "johntheripper" ;;
+        14) main_menu ;;
+        *) echo "Invalid choice. Please enter a number between 1 and 14." && sleep 2 ;;
+    esac
+}
+
 
 # Function to display custom command
 custom_command() {
